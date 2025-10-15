9745106137a77a4838f684f68afc8bc6e5a90909
gabriel4217 <gabrielyagofelipe17@gmail.com>
2025-10-15 17:01:13 -0300

    Add files via upload

--- footer.php
+++ footer.php
@@ -1,5 +1,37 @@
     <footer>
-        <p>Desenvolvido por <a href="https://osamostradinhos.com.br" target="_blank" rel="noopener">Os Amostradinhos</a></p>
+        <div class="container footer-menu">
+            <div>
+                <h1>O que fazer?</h1>
+                <ul> 
+                    <li >ACADEMIA</li>
+                    <li>ACADEMIAS</li>
+                    <li>ACAMPAMENTOS</li>
+                    <li>ACEITA ANIMAIS NO QUARTO </li>
+                    <li>ACEITA CARTOES DE CREDITO</li>
+                    <li>ACEITA CARTOES DE DEBITO</li>
+                    <li>ACEITA RESERVA</li>
+    
+                </ul>
+            </div>
+            <div>
+                <h1>O que fazer?</h1>
+                <ul> 
+                    <li >ACADEMIA</li>
+                    <li>ACADEMIAS</li>
+                    <li>ACAMPAMENTOS</li>
+                    <li>ACEITA ANIMAIS NO QUARTO </li>
+                    <li>ACEITA CARTOES DE CREDITO</li>
+                    <li>ACEITA CARTOES DE DEBITO</li>
+                    <li>ACEITA RESERVA</li>
+    
+                </ul>
+            </div>
+             <div>
+                <h1>O que fazer?</h1>
+                <img src="<?php echo get_template_directory_uri(); ?>/images/logo-footer.png" alt="">
+
+            </div>
+        </div>
+
     </footer>
-</body>
-</html>
+      
