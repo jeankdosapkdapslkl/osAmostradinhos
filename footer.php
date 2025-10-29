b0e4b26f59b5fd0e6b8c3122ea2b904c6acabd54
gabriel4217 <gabrielyagofelipe17@gmail.com>
2025-10-29 17:06:16 -0300

    Add files via upload

--- footer.php
+++ footer.php
@@ -1,37 +1,46 @@
     <footer>
         <div class="container footer-menu">
             
-            <div>
+            <div class="footer-item">
                 <h1>O que fazer?</h1>
                 <ul> 
-                    <li >ACADEMIA</li>
-                    <li>ACADEMIAS</li>
-                    <li>ACAMPAMENTOS</li>
-                    <li>ACEITA ANIMAIS NO QUARTO </li>
-                    <li>ACEITA CARTOES DE CREDITO</li>
-                    <li>ACEITA CARTOES DE DEBITO</li>
-                    <li>ACEITA RESERVA</li>
+                    <li><a href="#">academia</a></li>
+                    <li><a href="#">academias</a></li>
+                    <li><a href="#">acampamentos</a></li>
+                    <li><a href="#">aceita animais no quarto</a></li>
+                    <li><a href="#">aceita cartoes de credito</a></li>
+                    <li><a href="#">aceita cartoes  de debito</a></li>
+                    <li><a href="#">aceita reserva</a></li>
     
                 </ul>
             </div>
-            <div>
+
+            <div class="footer-item">
                 <h1>Sobre a Cidade</h1>
                 <ul> 
-                    <li >ACADEMIA</li>
-                    <li>ACADEMIAS</li>
-                    <li>ACAMPAMENTOS</li>
-                    <li>ACEITA ANIMAIS NO QUARTO </li>
-                    <li>ACEITA CARTOES DE CREDITO</li>
-                    <li>ACEITA CARTOES DE DEBITO</li>
-                    <li>ACEITA RESERVA</li>
+                    <li><a href="#">inicio</a></li>
+                    <li><a href="#">sobre a cidade</a></li>
+                    <li><a href="#">lendas e curiosidade</a></li>
+                    <li><a href="#">o que fazer</a> </li>
+                    <li><a href="#">eventos turisticos</a></li>
+                    <li><a href="#">roteiros municipais</a></li>
+                    <li><a href="#">roteiros intermunicipais</a></li>
+                    <li><a href="#">onde comer</a></li>
+                    <li><a href="#" >onde ficar</a></li>
+                  <li><a href="#">guia de serviços</a></li>
+                    <li><a href="#">cadastre sua empresa</a></li>
     
                 </ul>
             </div>
-             <div>
-                <h1>O que fazer?</h1>
-                <img src="<?php echo get_template_directory_uri(); ?>/images/logo-footer.png" alt="">
 
-            </div>
+             <div class="footer-item">
+               <h1>Município de Rio do Sul</h1>
+      
+            <?php
+                echo '<img src="' . get_template_directory_uri() . '/images/logo.png" alt="Logo do site">';
+                ?>
+             <p>Ir para o site da prefeitura</p>
+     </div>
         </div>
 
     </footer>
